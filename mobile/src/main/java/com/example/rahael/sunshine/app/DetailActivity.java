1550a4b39fa944a21bac0da2348e5a6c8e380350
Joe Abhishek <joeabhishek@gmail.com>
2015-02-03 13:05:59 -0500

    Midway through setting up the settings panel

--- mobile/src/main/java/com/example/rahael/sunshine/app/DetailActivity.java
+++ mobile/src/main/java/com/example/rahael/sunshine/app/DetailActivity.java
@@ -1,9 +1,17 @@
 package com.example.rahael.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static com.example.rahael.sunshine.app.R.id.detail_text;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -11,7 +19,12 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail2);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
@@ -31,9 +44,32 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            Intent intent = getActivity().getIntent();
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            if( intent != null && intent.hasExtra(Intent.EXTRA_TEXT) ){
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(detail_text))
+                        .setText(forecastStr);
+            }
+            return rootView;
+        }
     }
 }
