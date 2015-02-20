8c3bc2885f719669e73ff2b3233752c1c016a326
Joe Abhishek <joeabhishek@gmail.com>
2015-02-20 01:06:00 -0500

    Add notifications to the app

--- mobile/src/main/java/com/example/rahael/sunshine/app/MainActivity.java
+++ mobile/src/main/java/com/example/rahael/sunshine/app/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.example.rahael.sunshine.app.sync.SunshineSyncAdapter;
 
 public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
 
@@ -43,6 +44,8 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
+
+        SunshineSyncAdapter.initializeSyncAdapter(this);
     }
 
     @Override
